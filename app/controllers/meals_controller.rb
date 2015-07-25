9a5a5810b00aa2cd8a1b2a6e4e54a6cd5f422b4e
goodallt <goodallt@umich.edu>
2015-07-25 23:43:50 +0200

    updated meals controller and added show meals view

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,1 +1,45 @@
-meals_controller.rb
+class MealsController < ApplicationController
+
+	def show
+		@meal = Meal.find(params[:id])
+	end
+
+	def new
+		@meal = Meal.new
+	end
+
+	def create
+		@meal = Meal.new(meal_params)
+
+		if @meal.save
+			redirect_to meal_path(@meal)
+		else
+			render "new"
+		end
+	end
+
+	def edit
+		@meal = Meal.find(params[:id])
+	end
+
+	def update
+		@meal = Meal.find(params[:id])
+		if @meal.update(meal_params)
+			redirect_to meal_path(@meal)
+		else 
+			render "edit"
+		end
+	end
+
+	def destroy
+		@meal = Meal.find(params[:id])
+		@meal.destroy
+
+		redirect_to meals_path
+	end
+
+	private
+		def meal_params
+			params.require(:meal).permit(:name)
+		end
+end
